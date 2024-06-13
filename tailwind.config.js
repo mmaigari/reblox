38a73a0a7db844f90656f1fc260d3d4e7ae4713a
Mubarak Ibrahim Maigari <tha_rugged@yahoo.com>
2024-06-13 01:54:08 +0100

    New

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+
+    theme: {
+    extend: {
+      animation: {
+        border: 'border 4s ease infinite',
+      },
+      keyframes: {
+        border: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+    },
+  },
 }
+
