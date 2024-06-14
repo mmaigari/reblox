e8a45e7a05325f33831dd419f4cbdbff2701568b
Mubarak Ibrahim Maigari <tha_rugged@yahoo.com>
2024-06-14 03:12:00 +0100

    Heading to MVP

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,16 @@ module.exports = {
   ],
 
     theme: {
-    extend: {
-      animation: {
-        border: 'border 4s ease infinite',
-      },
-      keyframes: {
-        border: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
+      extend: {
+        animation: {
+          rotate: "rotate 10s linear infinite",
+        },
+        keyframes: {
+          rotate: {
+            "0%": { transform: "rotate(0deg) scale(10)" },
+            "100%": { transform: "rotate(-360deg) scale(10)" },
+          },
         },
       },
     },
-  },
 }
-
