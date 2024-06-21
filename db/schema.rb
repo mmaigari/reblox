cb789749553d229dcbbc05e2d436eb18a42a4cef
Mubarak Ibrahim Maigari <tha_rugged@yahoo.com>
2024-06-21 17:37:40 +0100

    best

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_143425) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_161329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_143425) do
   end
 
   create_table "contact_us", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.text "message"
