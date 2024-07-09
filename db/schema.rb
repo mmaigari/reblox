e5ff37ae9dca1c534904150d02625cd343650208
Mubarak Ibrahim Maigari <tha_rugged@yahoo.com>
2024-07-09 11:09:00 +0100

    hghyg

--- db/schema.rb
+++ db/schema.rb
@@ -121,7 +121,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_07_195115) do
 
   create_table "stayintheloops", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", unique: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
